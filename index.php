ca82975d9d8d05d305b926ba7eef79f8c0ccfc01
frankyfrankfrank <adam@webadamfrank.com>
2016-10-28 16:30:27 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<!--[if lte IE 6]><html class="preIE7 preIE8 preIE9"><![endif]-->
+<!--[if IE 7]><html class="preIE8 preIE9"><![endif]-->
+<!--[if IE 8]><html class="preIE9"><![endif]-->
+<!--[if gte IE 9]><!--><html><!--<![endif]-->
+  <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Doon South</title>
+  <meta name="author" content="name">
+  <meta name="description" content="Doon South Home Community">
+  <meta name="keywords" content="Doon South, Home, Home Builder">
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css">
+
+  <link rel="stylesheet" href="css/stylesheet.css" type="text/css">
+  </head>
+  <body>
+
+  <div class="container-fluid jumbotron">
+
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <img class="logo" src="img/logo-1-grey.svg" alt="Doon South - Naturally embraced living" />
+        <img class="ds-caret" src="img/graphic-silver-caret.svg" />
+      </div>
+    </div>
+
+  </div><!-- end JUMBOTRON-->
+
+  <div class="container-fluid explore">
+
+    <div class="row">
+
+      <div class="col-md-12 intro">
+        <h2>Explore</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+        </p>
+      </div>
+
+      <div class="amenity col-xs-6">
+        <img class="amenity-icon" src="img/amenity-parks.svg" alt="Parks Icon" />
+        <h3>Parks</h3>
+        <p>
+          Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum.
+        </p>
+      </div>
+
+      <div class="amenity col-xs-6">
+        <img class="amenity-icon" src="img/amenity-greenspace-and-marshland.svg" alt="Parks Icon" />
+        <h3>Greenspace</h3>
+        <p>
+          Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum.
+        </p>
+      </div>
+
+      <div class="amenity col-xs-6">
+        <img class="amenity-icon" src="img/amenity-trails.svg" alt="Parks Icon" />
+        <h3>Trails</h3>
+        <p>
+          Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum.
+        </p>
+      </div>
+
+      <div class="amenity col-xs-6">
+        <img class="amenity-icon" src="img/amenity-fishing.svg" alt="Parks Icon" />
+        <h3>Fishing</h3>
+        <p>
+          Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum.
+        </p>
+      </div>
+
+      <div class="amenity col-xs-6">
+        <img class="amenity-icon" src="img/amenity-culture.svg" alt="Parks Icon" />
+        <h3>Culture</h3>
+        <p>
+          Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum.
+        </p>
+      </div>
+
+      <div class="amenity col-xs-6">
+        <img class="amenity-icon" src="img/amenity-sports.svg" alt="Parks Icon" />
+        <h3>Sports</h3>
+        <p>
+          Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum.
+        </p>
+      </div>
+
+      <div class="amenity col-xs-6">
+        <img class="amenity-icon" src="img/amenity-golf.svg" alt="Parks Icon" />
+        <h3>Golf</h3></h3>
+        <p>
+          Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum.
+        </p>
+      </div>
+
+      <div class="amenity col-xs-6">
+        <img class="amenity-icon" src="img/amenity-shopping.svg" alt="Parks Icon" />
+        <h3>Shopping</h3>
+        <p>
+          Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum.
+        </p>
+      </div>
+
+      <div class="amenity col-xs-6">
+        <img class="amenity-icon" src="img/amenity-school.svg" alt="Parks Icon" />
+        <h3>Schools</h3>
+        <p>
+          Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum.
+        </p>
+      </div>
+
+      <div class="col-xs-12 text-center">
+        <img class="ds-caret" src="img/graphic-gold-caret.svg" />
+      </div>
+
+    </div>
+
+  </div><!-- End EXPLORE -->
+
+  <div class="container-fluid connect">
+
+    <div class="row">
+
+      <div class="col-xs-12">
+        <div class="intro">
+          <h2>Connect</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+          </p>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="row">
+
+      <div class="col-xs-6">
+        <h3>Flora</h3>
+        <ul>
+          <li>Lorem ipsum</li>
+          <li>Dolor sit</li>
+          <li>Consectetuer</li>
+          <li>Adipiscing elit</li>
+          <li>Diam</li>
+          <li>Nonummy nibh</li>
+          <li>Tincidunt</li>
+        </ul>
+      </div>
+
+      <div class="col-xs-6">
+        <h3>Fauna</h3>
+        <ul>
+          <li>Lorem ipsum</li>
+          <li>Dolor sit</li>
+          <li>Consectetuer</li>
+          <li>Adipiscing elit</li>
+          <li>Diam</li>
+          <li>Nonummy nibh</li>
+          <li>Tincidunt</li>
+        </ul>
+      </div>
+
+      <div class="col-xs-12 text-center">
+        <img class="ds-caret" src="img/graphic-purple-caret.svg" />
+      </div>
+
+    </div>
+
+  </div><!-- end CONNECT -->
+
+  <div class="container-fluid live">
+
+    <div class="row">
+
+      <div class="col-xs-12 intro">
+        <h2>Live</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+        </p>
+      </div>
+
+    </div>
+
+    <div class="viewer">
+
+    <div class="row home-type text-center">
+
+      <div class="col-xs-12">
+        <div id="villas" class="active">
+          Villas
+        </div>
+        <div id="towns">
+          Towns
+        </div>
+      </div>
+
+    </div>
+
+    <div class="row floor-ind text-center">
+      <div class="col-xs-12">
+        <div class="pill active"></div>
+        <div class="pill"></div>
+      </div>
+    </div>
+
+    <div class="row unit">
+      <div class="col-xs-12">
+        <img src="img/graphic-villa-upland.svg" />
+        <h4><strong>Upper</strong> Unit</h4>
+        <h3>Upland</h3>
+        <ul class="details">
+          <li>
+            1691 Sq. Ft.
+          </li>
+          <li>
+            3 Bed
+          </li>
+          <li>
+            2 Bath
+          </li>
+        </ul>
+        <h5 class="price"><sup>$</sup>200,000</h5>
+      </div>
+    </div>
+
+    <div class="row floorplan">
+      <div class="col-xs-10 col-xs-offset-1">
+        <img class="img img-responsive" src="img/floorplan-upland-1.png" alt="uploand floorplan 1"/>
+      </div>
+    </div>
+
+    </div>
+
+  </div><!-- end LIVE -->
+
+  <div class="container-fluid siteplan">
+
+    <div class="row">
+
+      <div class="intro col-xs-12">
+        <h2>Siteplan</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+        </p>
+        <ul class="legend">
+          <li><div class="swatch swatch-villas"></div>Villas</li>
+          <li><div class="swatch swatch-towns"></div>Towns</li>
+        </ul>
+      </div>
+
+    </div>
+
+  </div>
+
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.5.3/modernizr.min.js" type="text/javascript"></script>
+
+  <!-- Typekit  -->
+  <script src="https://use.typekit.net/pyo8kfd.js"></script>
+  <script>try{Typekit.load({ async: true });}catch(e){}</script>
+
+  <!-- Bootstrap JS -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>
+
+  </body>
+</html>
